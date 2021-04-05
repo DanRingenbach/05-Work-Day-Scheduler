8647c2834c225691eac6795431e65ea8659ff3a1
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-04 20:33:06 -0400

    final commit

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # 05-Work-Day-Scheduler
 ## Description
-The task of this project was to create a simple coding quiz using vanilla Javascript code.  I created the index.html, styles.css, and script.js from scratch.  
+The task of this project was to create a simple daily scheduling web application.  Starter code for the css and the html were given.  The Javascript file was written entirely from scratch.  Moment JS was used to make the application time-dependant. Appointements/user input is saved locally and displayed when the user opens the application.
 
 **Links**
 
-* [Deployed Webpage](https://danringenbach.github.io/04-Code-Quiz/ "Deployed Webpage")
-* [Repository Link](https://github.com/DanRingenbach/04-Code-Quiz "Repository Link")
+* [Deployed Webpage](https://danringenbach.github.io/05-Work-Day-Scheduler/ "Deployed Webpage")
+* [Repository Link](https://github.com/DanRingenbach/05-Work-Day-Scheduler "Repository Link")
 
 
 **Screenshot**
  
  ![alt text](screenshot.png)
  
- ![alt text](screenshot2.png)
+ 
--- script.js
+++ script.js
@@ -1,72 +1,48 @@
-var displayDate = $("currentDay");
-var hour = moment().format('h')
-var saveBtn = $('.saveBtn')
-var userInput = $('.col-1')
-row = $('.row')
+displayDate = $('#currentDay')
+saveBtn = $('.saveBtn')
+currentHour = Number(moment().format('HH'))
 
 $(document).ready(function () {
-    event.preventDefault(); 
-        $("#9AMText").append(localStorage.getItem("am9"));
-        $("#10AMText").append(localStorage.getItem("am10"));
-        $("#11AMText").append(localStorage.getItem("am11"));
-        $("#12PMText").append(localStorage.getItem("pm12"));
-        $("#1PMText").append(localStorage.getItem("pm1"));
-        $("#2PMText").append(localStorage.getItem("pm2"));
-        $("#3PMText").append(localStorage.getItem("pm3"));
-        $("#4PMText").append(localStorage.getItem("pm4"));
-        $("#5PMText").append(localStorage.getItem("pm5"));
-    
-        var todayDate = moment().format('dddd, MMM Do YYYY');
+    var todayDate = moment().format('dddd, MMM Do YYYY');
     displayDate.html(todayDate);
-
-    saveBtn.on('click', function () {
+    
+    var loadLocalStorage = function () {
+    $('input[name="hour-9-text"]').val(localStorage.getItem('9'));
+    $('input[name="hour-10-text"]').val(localStorage.getItem('10'));
+    $('input[name="hour-11-text"]').val(localStorage.getItem('11'));
+    $('input[name="hour-12-text"]').val(localStorage.getItem('12'));
+    $('input[name="hour-13-text"]').val(localStorage.getItem('13'));
+    $('input[name="hour-14-text"]').val(localStorage.getItem('14'));
+    $('input[name="hour-15-text"]').val(localStorage.getItem('15'));
+    $('input[name="hour-16-text"]').val(localStorage.getItem('16'));
+    $('input[name="hour-17-text"]').val(localStorage.getItem('17'));
+    }
+    
+    loadLocalStorage();
+    
+    $('.row').each(function(){
+        var timeBlock = Number($(this).children()[0].getAttribute('id')) 
+        var timeSpan = $(this).children()[1]
         
-        var am9 = ($('#9AM').text())
-        var am10 = ($('#10AM').text())
-        var am11 = ($('#10AM').text())
-        var pm12 = ($('#10AM').text())
-        var pm1 = ($('#10AM').text())
-        var pm2 = ($('#10AM').text())
-        var pm3 = ($('#10AM').text())
-        var pm4 = ($('#10AM').text())
-        var pm5 = ($('#10AM').text())
-        localStorage.setItem(am9, ($('#9AMText').val()))
-        localStorage.setItem(am10, ($('#10AMText').val()))
-        localStorage.setItem(am11, ($('#11AMText').val()))
-        localStorage.setItem(pm12, ($('#12PMText').val()))
-        localStorage.setItem(pm1, ($('#1PMText').val()))
-        localStorage.setItem(pm2, ($('#2PMText').val()))
-        localStorage.setItem(pm3, ($('#3PMText').val()))
-        localStorage.setItem(pm4, ($('#4PMText').val()))
-        localStorage.setItem(pm5, ($('#5PMText').val()))
-        
-
-
-
-
-
-
-        // var value = userInput.val()
-        // localStorage.setItem(keyHour,value);
-        // console.log(value)
+        if (currentHour === timeBlock) {
+            $(timeSpan).addClass('present')
+        } else if (currentHour > timeBlock) {
+            $(timeSpan).addClass('past')
+        } else if (currentHour < timeBlock) {
+            $(timeSpan).addClass('future')
+        }
+    
     })
 
-    for (hour = 9; hour <= 17; hour++) {
-        // index for array use offset from hour
-        index = hour - 9;
-        console.log(hour)
+    saveBtn.on('click', function (event) {
+        userInput = ($(this).siblings('input').val())
+        event.preventDefault();
+        var keyTime = $(this).siblings('span').attr('id')
+        localStorage.setItem(keyTime, userInput)
 
-
-
-
-    }
+    })
+   
 })
-
-
-
-
-
-
 
 
 
